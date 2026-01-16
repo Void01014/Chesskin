d9a4f84fb8918a5bb083a0c4f9a0df9dc1c29282
Omar EL FILALI <omarelfiie2007@gmail.com>
2026-01-16 10:24:02 +0100

    added the 'Cahier de Charge'

--- main.js
+++ main.js
@@ -4,5 +4,11 @@ const game = new Game;
 
 game.board.render();
 game.handleSquareClick(7, 1);
-game.handleSquareClick(5, 1);
+game.handleSquareClick(5, 0);
+game.board.render();
+game.handleSquareClick(0, 1);
+game.handleSquareClick(2, 2);
+game.board.render();
+game.handleSquareClick(5, 0);
+game.handleSquareClick(3, 1);
 game.board.render();
